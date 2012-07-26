f4dee76ef820c5ad8cf1e70843cb238151b89dbb
Greg Daues <daues@lsst14.ncsa.illinois.edu>
2012-07-26 12:02:37 -0500

    Support idsPerJob through configuration

--- dagutils/dag/sdss/photo/local_data.py
+++ dagutils/dag/sdss/photo/local_data.py
@@ -91,6 +91,7 @@ root.workflow["association"].task["isr"].workerJob.script.keywords["LSST_HOME"] 
 
 root.workflow["association"].task["isr"].dagGenerator.dagName = "S2012Pipe"
 root.workflow["association"].task["isr"].dagGenerator.script = "$DATAREL_DIR/dagutils/dag/sdss/photo/dag/scripts/newgenerateDag.py"
+root.workflow["association"].task["isr"].dagGenerator.idsPerJob=12
 # root.workflow["association"].task["isr"].dagGenerator.input = "$DATAREL_DIR/dagutils/dag/sdss/photo/dag/input/80000.sdss.input"
 
 # root.workflow["association"].task["isr"].dagGenerator.input = "$DATAREL_DIR/dagutils/dag/sdss/photo/dag/input/19260.sdss.input"
